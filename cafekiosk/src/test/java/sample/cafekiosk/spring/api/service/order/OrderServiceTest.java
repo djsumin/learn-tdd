d80520cbffc7cda7d3c9e170ff5bfab5309fbc71
djsumin <judy3504@naver.com>
2024-10-31 22:05:14 +0900

    business layer #3 with stock

--- cafekiosk/src/test/java/sample/cafekiosk/spring/api/service/order/OrderServiceTest.java
+++ cafekiosk/src/test/java/sample/cafekiosk/spring/api/service/order/OrderServiceTest.java
@@ -15,6 +15,8 @@ import sample.cafekiosk.spring.domain.orderproduct.OrderProductRepository;
 import sample.cafekiosk.spring.domain.product.Product;
 import sample.cafekiosk.spring.domain.product.ProductRepository;
 import sample.cafekiosk.spring.domain.product.ProductType;
+import sample.cafekiosk.spring.domain.stock.Stock;
+import sample.cafekiosk.spring.domain.stock.StockRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -23,10 +25,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.tuple;
 import static org.junit.jupiter.api.Assertions.*;
 import static sample.cafekiosk.spring.domain.product.ProductSellingStatus.*;
-import static sample.cafekiosk.spring.domain.product.ProductType.HANDMADE;
+import static sample.cafekiosk.spring.domain.product.ProductType.*;
 
 @ActiveProfiles("test")
-//@Transactional
+@Transactional
 @SpringBootTest
 //transaction이 달려있지않아서 자동 롤백 안되므로 데이터 클렌징 해줘야햄
 //@DataJpaTest
@@ -44,46 +46,49 @@ class OrderServiceTest {
     @Autowired
     private OrderProductRepository orderProductRepository;
 
+    @Autowired
+    private StockRepository stockRepository;
+
 
     //데이터 클렌징 작업 : 테스트 끝날 때마다 데이터 클렌즈
-    @AfterEach
-    void tearDown() {
-//        productRepository.deleteAll();
-        orderProductRepository.deleteAllInBatch();
-        productRepository.deleteAllInBatch();
-        orderRepository.deleteAllInBatch();
-    }
+//    @AfterEach
+//    void tearDown() {
+////        productRepository.deleteAll();
+//        orderProductRepository.deleteAllInBatch();
+//        productRepository.deleteAllInBatch();
+//        orderRepository.deleteAllInBatch();
+//    }
 
     @DisplayName("주문번호 리스트를 받아 주문을 생성한다.")
     @Test
     void createOrder(){
-        //given
-        Product product1 = createProduct(HANDMADE, "001", 1000);
-        Product product2 = createProduct(HANDMADE, "002", 3000);
-        Product product3 = createProduct(HANDMADE, "003", 5000);
+    //given
+    Product product1 = createProduct(HANDMADE, "001", 1000);
+    Product product2 = createProduct(HANDMADE, "002", 3000);
+    Product product3 = createProduct(HANDMADE, "003", 5000);
 
-        productRepository.saveAll(List.of(product1, product2, product3));
+    productRepository.saveAll(List.of(product1, product2, product3));
 
-        OrderCreateRequest request = OrderCreateRequest.builder()
-                .productNumbers(List.of("001", "002"))
-                .build();
-        //when
-        LocalDateTime registeredDateTime = LocalDateTime.now();
-        OrderResponse orderResponse = orderService.createOrder(request, registeredDateTime);
+    OrderCreateRequest request = OrderCreateRequest.builder()
+    .productNumbers(List.of("001", "002"))
+    .build();
+    //when
+    LocalDateTime registeredDateTime = LocalDateTime.now();
+    OrderResponse orderResponse = orderService.createOrder(request, registeredDateTime);
 
-        //then
-        assertThat(orderResponse.getId()).isNotNull();
-        assertThat(orderResponse)
-                .extracting("registeredDateTime", "totalPrice")
-                .contains(registeredDateTime, 4000);
-        assertThat(orderResponse.getProducts()).hasSize(2)
-                .extracting("productNumber", "price")
-                .containsExactlyInAnyOrder(
-                        tuple("001", 1000),
-                        tuple("002", 3000)
-                );
+    //then
+    assertThat(orderResponse.getId()).isNotNull();
+    assertThat(orderResponse)
+    .extracting("registeredDateTime", "totalPrice")
+    .contains(registeredDateTime, 4000);
+    assertThat(orderResponse.getProducts()).hasSize(2)
+    .extracting("productNumber", "price")
+        .containsExactlyInAnyOrder(
+                    tuple("001", 1000),
+                    tuple("002", 3000)
+            );
 
-    }
+        }
 
     @DisplayName("중복되는 상품번호 리스트로 주문을 생성할 수 있다.")
     @Test
@@ -116,6 +121,51 @@ class OrderServiceTest {
                         tuple("001", 1000),
                         tuple("001", 1000)
                 );
+    }
+
+    @DisplayName("재고와 관련된 상품이 포함되어 있는 주문번호 리스트를 받아 주문을 생성한다.")
+    @Test
+    void createOrderWithStock(){
+        //given
+        LocalDateTime registeredDateTime = LocalDateTime.now();
+        Product product1 = createProduct(BOTTLE, "001", 1000);
+        Product product2 = createProduct(BAKERY, "002", 3000);
+        Product product3 = createProduct(HANDMADE, "003", 5000);
+
+        productRepository.saveAll(List.of(product1, product2, product3));
+
+        Stock stock =  Stock.create("001", 2);
+        Stock stock2 =  Stock.create("002", 2);
+        stockRepository.saveAll(List.of(stock, stock2));
+
+        OrderCreateRequest request = OrderCreateRequest.builder()
+                .productNumbers(List.of("001","001", "002", "003"))
+                .build();
+        //when
+        OrderResponse orderResponse = orderService.createOrder(request, registeredDateTime);
+
+        //then
+        assertThat(orderResponse.getId()).isNotNull();
+        assertThat(orderResponse)
+                .extracting("registeredDateTime", "totalPrice")
+                .contains(registeredDateTime, 10000);
+        assertThat(orderResponse.getProducts()).hasSize(4)
+                .extracting("productNumber", "price")
+                .containsExactlyInAnyOrder(
+                        tuple("001", 1000),
+                        tuple("001", 1000),
+                        tuple("002", 3000),
+                        tuple("003", 5000)
+                );
+
+        List<Stock> stocks = stockRepository.findAll();
+        assertThat(stocks).hasSize(2)
+                .extracting("productNumber", "quantity")
+                .containsExactlyInAnyOrder(
+                        tuple("001", 0),
+                        tuple("002", 1)
+                );
+
     }
 
 
